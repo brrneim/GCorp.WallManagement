88422786da4c47f2e4dbd52e45129c6a1bb89002
Mehmet Sefa Guller <gullerm@emea.corpdir.net>
2023-11-13 22:09:12 +0300

    Work Controller is added and retrieve all works.

---
+++ GloboTicket.TicketManagement.Api/Controllers/WorkController.cs
@@ -0,0 +1,31 @@
+﻿using GloboTicket.TicketManagement.Application.Features.CategoryTypes.Queries.GetCategoryTypeList;
+using GloboTicket.TicketManagement.Application.Features.Works.Queries.GetWorkList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public WorkController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //[Authorize]
+        [HttpGet("all", Name = "GetAllWorks")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<WorkListVm>>> GetAllCategories()
+        {
+            var dtos = await _mediator.Send(new GetWorkListQuery());
+            return Ok(dtos);
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Models/Dtos/WorkDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Models.Dtos
+{
+    public class WorkDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public Guid CustomerId { get; set; }
+        public int CityId { get; set; }
+        public int CountyId { get; set; }
+        public string LocationX { get; set; }
+        public string LocationY { get; set; }
+        public Guid DealCustomerId { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public bool IsActive { get; set; }
+        public Guid StateId { get; set; }
+    }
+}
