d94e101550aa76b0d264bb693a80909008262121
Mehmet Sefa Guller <gullerm@emea.corpdir.net>
2024-03-31 04:36:22 +0300

    Customer Message servisleri tamamlandı.

---
+++ GloboTicket.TicketManagement.Application/Features/CustomerMessages/Commands/CreateCustomerMessageCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace GloboTicket.TicketManagement.Application.Features.CustomerMessages.Commands
+{
+    public class CreateCustomerMessageCommandHandler : IRequestHandler<CreateCustomerMessageCommand, CreateCustomerMessageCommandResponse>
+    {
+        private readonly IAsyncRepository<CustomerMessage> _workRepository;
+        private readonly IMapper _mapper;
+
+        public CreateCustomerMessageCommandHandler(IMapper mapper, IAsyncRepository<CustomerMessage> workRepository)
+        {
+            _mapper = mapper;
+            _workRepository = workRepository;
+        }
+
+        public async Task<CreateCustomerMessageCommandResponse> Handle(CreateCustomerMessageCommand request, CancellationToken cancellationToken)
+        {
+            var createWorkCommandResponse = new CreateCustomerMessageCommandResponse();
+
+            var validator = new CreateCustomerMessageCommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Count > 0)
+            {
+                createWorkCommandResponse.Success = false;
+                createWorkCommandResponse.ValidationErrors = new List<string>();
+                foreach (var error in validationResult.Errors)
+                {
+                    createWorkCommandResponse.ValidationErrors.Add(error.ErrorMessage);
+                }
+            }
+            if (createWorkCommandResponse.Success)
+            {
+                var customerMessage = new CustomerMessage()
+                {
+                    Message = request.Message,
+                    CustomerReceiverId = request.CustomerRecieverId,
+                    CustomerSenderId = request.CustomerSenderId,
+                    WorkId = request.WorkId,
+                    IsRead = request.IsRead
+                };
+                customerMessage = await _workRepository.AddAsync(customerMessage);
+                createWorkCommandResponse.CreateCustomerMessage = _mapper.Map<CreateCustomerMessageDto>(customerMessage);
+            }
+
+            return createWorkCommandResponse;
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/CustomerMessages/Commands/CreateCustomerMessageCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using GloboTicket.TicketManagement.Application.Features.Works.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Features.CustomerMessages.Commands
+{
+    public class CreateCustomerMessageCommandValidator : AbstractValidator<CreateCustomerMessageCommand>
+    {
+        public CreateCustomerMessageCommandValidator()
+        {
+            RuleFor(p => p.CustomerRecieverId)
+                .NotEmpty().WithMessage("{CustomerRecieverId} is required.")
+                .NotNull();
+            RuleFor(p => p.CustomerSenderId)
+                .NotEmpty().WithMessage("{CustomerSenderId} is required.")
+                .NotNull();
+        }
+    }
+}
